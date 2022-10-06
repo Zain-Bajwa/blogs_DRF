819d26457c050d55701ad31cdb2c68ddf86a648e
Hafiz Zain Ul Haq <zainalibajwa5@gmail.com>
2022-10-06 14:58:55 +0500

    feat: add swagger

--- blogs_drf/urls.py
+++ blogs_drf/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(title="Pastebin API", default_version='v1'),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
