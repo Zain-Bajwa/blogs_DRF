3b4c6564265c75e1fb2acf39119c8988d551f6e7
Hafiz Zain Ul Haq <zainalibajwa5@gmail.com>
2022-10-06 16:18:46 +0500

    fix: update requirements.txt and fix indentation

--- blogs_drf/urls.py
+++ blogs_drf/urls.py
@@ -20,13 +20,23 @@ from django.urls import path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+
+# pylint: disable=invalid-name
+chema_view = get_schema_view(
     openapi.Info(title="Pastebin API", default_version='v1'),
     public=True,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(
+            r'^swagger(?P<format>\.json|\.yaml)$',
+            chema_view.without_ui(cache_timeout=0),
+            name='schema-json'
+    ),
+    re_path(
+            r'^swagger/$',
+            chema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'
+    ),
 ]
